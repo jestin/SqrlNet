b3c5350f32bf2a8f70623c1d685915e625fdd671
Jestin Stoffel <jestin.stoffel@gmail.com>
2014-02-11 22:15:35 -0600

    wrote some unit tests to make sure that my pbkdf handler still works like GRC's when given different salts.  It does.

--- SqrlNetTests/CryptSharpPbkdfHandlerTests.cs
+++ SqrlNetTests/CryptSharpPbkdfHandlerTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using SqrlNet.Crypto.CryptSharp;
 using System;
+using System.Linq;
 using SqrlNet;
 
 namespace SqrlNetTests
@@ -108,6 +109,7 @@ namespace SqrlNetTests
 
 		// Output Key: 2f30b9d4e5c48056177ff90a6cc9da04b648a7e8451dfa60da56c148187f6a7d
 		[Test]
+		[Ignore] // this test is valid, but it takes too long to run.  Uncomment to run.
 		public void GeneratePasswordKey_password_zeros_123()
 		{
 			var hash = _handler.GeneratePasswordKey("password", Utility.GetZeroBytes(32), 123);
@@ -141,7 +143,7 @@ namespace SqrlNetTests
 			Assert.AreEqual("532bcc911c16df81996258158de460b2e59d9a86531d59661da5fbeb69f7cd54", hex);
 		}
 
-		// enscrypt password 0000000000000000000000000000000000000000000000000000000000000000 1i
+		// enscrypt password 0000000000000000000000000000000000000000000000000000000000000000 2i
   		// Password: <null>
 		// Salt: 0000000000000000000000000000000000000000000000000000000000000000
 		// Iterations: 123
@@ -157,6 +159,93 @@ namespace SqrlNetTests
 			Console.Error.WriteLine(hex);
 
 			Assert.AreEqual("2d516e99bceb1f49e4dc02217ffc6bac28ea1a9b2d67c1dabd85185163ffe2de", hex);
+		}
+
+		// enscrypt password 0000000000000000000000000000000000000000000000000000000000000000 10i
+  		// Password: <null>
+		// Salt: 0000000000000000000000000000000000000000000000000000000000000000
+		// Iterations: 123
+
+		// Output Key: ba6006e4c23262a8cc7f242e110cc644e7ad8237e2b0a2265fbe6d51148c27b3
+		[Test]
+		public void GeneratePasswordKey_password_zeros_10()
+		{
+			var hash = _handler.GeneratePasswordKey("password", Utility.GetZeroBytes(32), 10);
+
+			var hex = BitConverter.ToString(hash).Replace("-","").ToLower();
+
+			Console.Error.WriteLine(hex);
+
+			Assert.AreEqual("ba6006e4c23262a8cc7f242e110cc644e7ad8237e2b0a2265fbe6d51148c27b3", hex);
+		}
+
+		// enscrypt password 0000000000000000000000000000000000000000000000000000000000000000 1i
+  		// Password: <null>
+		// Salt: 0000000000000000000000000000000000000000000000000000000000000000
+		// Iterations: 123
+
+		// Output Key: 7e7aa208a0fdc0c87dbe5a34f2180b1ecd77801d7e7a98bca020e3ba0a082f8a
+		[Test]
+		public void GeneratePasswordKey_password_532bcc911c16df81996258158de460b2e59d9a86531d59661da5fbeb69f7cd54_1()
+		{
+			var salt = StringToByteArray("532bcc911c16df81996258158de460b2e59d9a86531d59661da5fbeb69f7cd54");
+			var hash = _handler.GeneratePasswordKey("password", salt, 1);
+
+			var hex = BitConverter.ToString(hash).Replace("-","").ToLower();
+
+			Console.Error.WriteLine(hex);
+
+			Assert.AreEqual("7e7aa208a0fdc0c87dbe5a34f2180b1ecd77801d7e7a98bca020e3ba0a082f8a", hex);
+		}
+
+		// enscrypt password 0000000000000000000000000000000000000000000000000000000000000000 2i
+  		// Password: <null>
+		// Salt: 0000000000000000000000000000000000000000000000000000000000000000
+		// Iterations: 123
+
+		// Output Key: 87bc58dcc3d74b392a53632a70b4cd65c0573a0af88140eba7f0401678e3d199
+		[Test]
+		public void GeneratePasswordKey_password_2d516e99bceb1f49e4dc02217ffc6bac28ea1a9b2d67c1dabd85185163ffe2de_2()
+		{
+			var salt = StringToByteArray("2d516e99bceb1f49e4dc02217ffc6bac28ea1a9b2d67c1dabd85185163ffe2de");
+			var hash = _handler.GeneratePasswordKey("password", salt, 2);
+
+			var hex = BitConverter.ToString(hash).Replace("-","").ToLower();
+
+			Console.Error.WriteLine(hex);
+
+			Assert.AreEqual("87bc58dcc3d74b392a53632a70b4cd65c0573a0af88140eba7f0401678e3d199", hex);
+		}
+
+		// enscrypt password 0000000000000000000000000000000000000000000000000000000000000000 10i
+  		// Password: <null>
+		// Salt: 0000000000000000000000000000000000000000000000000000000000000000
+		// Iterations: 123
+
+		// Output Key: a15abed469fa6e28742430bbc5cd954321b00ba368e26b55fea949f1f8c1f2b0
+		[Test]
+		public void GeneratePasswordKey_password_ba6006e4c23262a8cc7f242e110cc644e7ad8237e2b0a2265fbe6d51148c27b3_10()
+		{
+			var salt = StringToByteArray("ba6006e4c23262a8cc7f242e110cc644e7ad8237e2b0a2265fbe6d51148c27b3");
+			var hash = _handler.GeneratePasswordKey("password", salt, 10);
+
+			var hex = BitConverter.ToString(hash).Replace("-","").ToLower();
+
+			Console.Error.WriteLine(hex);
+
+			Assert.AreEqual("a15abed469fa6e28742430bbc5cd954321b00ba368e26b55fea949f1f8c1f2b0", hex);
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		public static byte[] StringToByteArray(string hex)
+		{
+			return Enumerable.Range(0, hex.Length)
+				.Where(x => x % 2 == 0)
+				.Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+				.ToArray();
 		}
 
 		#endregion
